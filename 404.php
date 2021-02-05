2acd97f88f1e280ea31d074c8e9d2f5d8c4d9e7d
Starman <jxxclj@gmail.com>
2021-02-05 23:26:18 +0800

    Update 404.php

--- 404.php
+++ 404.php
@@ -2,15 +2,28 @@
 /**
  * post.php
  * 
- * 404
+ * 文章页
  * 
- * @author      FC
+ * @author      Jstar
  */
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 $this->need('includes/head.php');
-$this->need('includes/header.php');
 ?>
-
+<link rel="stylesheet" href="<?php Utils::indexTheme('assets/main.css'); ?>" />
 <!--页面主要内容-->
 
-<?php $this->need('includes/footer.php'); ?>
+
+<div class="mainContainer bring">
+	<!-- header -->
+	<?php $this->need('includes/header.php'); ?>
+	<!-- header -->
+
+	<!-- 主体 -->
+	<div class="indiv blue bodyother">
+		<h1>404</h1>
+	</div>
+
+	<div class="indiv blue">
+		<?php $this->need('includes/footer.php'); ?>
+	</div>
+</div>
