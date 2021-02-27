900902ee1a64de1f0a08e0bb95ba9483d525a447
Starman <jxxclj@gmail.com>
2021-02-27 23:46:51 +0800

    不属于这个时代的愚者啊~

--- FC/includes/header.php
+++ FC/includes/header.php
@@ -12,13 +12,13 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 
 <link rel="stylesheet" href="<?php Utils::indexTheme('assets/header.css'); ?>" />
 
-<div class="indiv blue headother">
-	<a class="btn" href="<?php $this->options->siteUrl(); ?>"><?php _e('首页'); ?></a>
-	<?php $this->widget('Widget_Contents_Page_List')->to($pages); ?>
-	<?php while($pages->next()): ?>
-		<a class="btn" href="<?php $pages->permalink(); ?>" title="<?php $pages->title(); ?>"><?php $pages->title(); ?></a>
-	<?php endwhile; ?>
-<!-- 	<a class="btn">
-		首 页
-	</a> -->
+<div class="indiv white headother font_style">
+	<ol class="header_list">
+		<li><a class="header_pages_title" href="<?php $this->options->siteUrl(); ?>"><?php _e('首页'); ?></a></li>
+		<?php $this->widget('Widget_Contents_Page_List')->to($pages); ?>
+		<?php while($pages->next()): ?>
+			<li><a class="header_pages_title" href="<?php $pages->permalink(); ?>" title="<?php $pages->title(); ?>"><?php $pages->title(); ?></a></li>
+		<?php endwhile; ?>
+	</ol>
+	
 </div>
